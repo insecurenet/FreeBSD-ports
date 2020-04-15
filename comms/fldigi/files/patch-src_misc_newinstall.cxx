049140a14ce59c46a173f8918bcb7c222880973b
insecurenet <61785125+insecurenet@users.noreply.github.com>
2020-04-15 20:36:34 +0200

    .

---
+++ comms/fldigi/files/patch-src_misc_newinstall.cxx
@@ -0,0 +1,20 @@
+--- src/misc/newinstall.cxx.orig	2017-10-28 00:24:34 UTC
++++ src/misc/newinstall.cxx
+@@ -67,7 +67,7 @@ text[3] = " btu <NAME> <CALL> de <MYCALL
+ 
+ label[4] = "SK @||";
+ text[4] = "\n\
+-tnx fer QSO <NAME>, 73, God bless.\n\
++tnx fer QSO <NAME>, 73.\n\
+ <ZDT> <CALL> de <MYCALL> sk\n\
+ <RX>";
+ 
+@@ -84,7 +84,7 @@ Age:   \n\
+ Rig:   \n\
+ Pwr:   \n\
+ Ant:   \n\
+-OS:    Linux\n\
++OS:    FreeBSD\n\
+ Soft:  <VER>\n\
+ Web:   \n\
+ Email: ";
